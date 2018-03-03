4f8424ee3a9bb48f0bb405b2d14e1d647dff7ead
13871359417 <784257005@qq.com>
2018-03-04 02:26:37 +0800

    提交测试

--- BaseFrame-Login/src/main/java/cn/com/BaseFrame/controller/LoginController.java
+++ BaseFrame-Login/src/main/java/cn/com/BaseFrame/controller/LoginController.java
@@ -32,7 +32,7 @@ public class LoginController extends BaseController {
     @RequestMapping(value = "/loginHandler", method = RequestMethod.GET)
     @ResponseBody
     public String loginHandler(HttpServletRequest request, HttpServletResponse response) {
-        //直接跳转到对应的jsp,返回登录页面
+        //直接跳转到对应的jsp,返回登录页面。
 
         BaseResultModel resultModel = invokeMethod(LoginService.class,"login",new BaseParamModel());
 
